1aaf0523ef32c0a8a2418d8b2955e76e5cbda7f2
Valentín Gutierrez Losada <vgutierrezl@cirsa.com>
2020-10-06 11:05:37 +0200

    Review 1 (feat) pagination

--- src/app/components/product/product-list/product-list.component.ts
+++ src/app/components/product/product-list/product-list.component.ts
@@ -11,24 +11,49 @@ import { ProductViewModel } from '../../product/product.model';
 })
 export class ProductListComponent implements OnInit {
     public products$: Observable<ProductViewModel[]>;
-    
+    public loading: boolean = true;
+    public productsDisplayed: number;
+    public maxProductsDisplayed: number;
+
     constructor(
         private http: HttpClient,
         private storeService: StoreService
     ) { }
 
     ngOnInit(): void {
-
+        this.loading = true;
         this.http.get<any>('https://frontend-tech-test-data.s3.eu-west-1.amazonaws.com/items.json')
             .subscribe(_res => {
                 _res.items.forEach(element => {
                     let product = new ProductViewModel(element);
                     this.storeService.addProduct(product);
                 });
-
                 this.products$ = this.storeService.getProductList();
+                this.initializeProductsDisplayed();
+                this.loading = false;
             });
 
+    }
+
+    public loadMoreProducts() {
+      this.loading = true;
+      setTimeout(() => {
+        if (this.productsDisplayed <= this.maxProductsDisplayed) {
+          this.productsDisplayed = this.productsDisplayed + 5;
+        }
+        this.loading = false;
+      }, 500);
+    }
+
+    private initializeProductsDisplayed(): void {
+      let productsSubscription = this.products$.subscribe(
+        (products: ProductViewModel[]) => {
+          if(products.length !== 0) {
+            this.productsDisplayed = 5;
+            this.maxProductsDisplayed = products.length;
+          }
+        })
+      productsSubscription.unsubscribe();
     }
 
 }
